877973ce1d0364702dde0713eedd21b91248f9e2
June <khazoda@gmail.com>
2025-03-03 14:33:50 +0000

    examplemod -> bronze refactor

---
+++ common/src/main/java/com/khazoda/bronze/Constants.java
@@ -0,0 +1,11 @@
+package com.khazoda.bronze;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "bronze";
+	public static final String MOD_NAME = "Bronze";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
