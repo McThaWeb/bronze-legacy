71c548964b12efb1224f178f864cb5cf96e93c5f
June|Khazoda <khazoda@gmail.com>
2025-09-16 19:32:04 +0100

    ore sound type fix (impl. https://github.com/Khazoda/bronze/commit/7f790967f2a1b18efd54a7afa092677c9bfffd2b)

--- common/src/main/java/com/khazoda/bronze/block/DeepslateTinOre.java
+++ common/src/main/java/com/khazoda/bronze/block/DeepslateTinOre.java
@@ -7,6 +7,6 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class DeepslateTinOre extends Block {
   public DeepslateTinOre(ResourceKey<Block> id) {
-    super(BlockBehaviour.Properties.of().strength(3.5f).sound(SoundType.METAL).requiresCorrectToolForDrops().setId(id));
+    super(BlockBehaviour.Properties.of().strength(3.5f).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops().setId(id));
   }
 }
